c9a1d347132f5c4fb9947b82412183295626919c
Aidan Crooks <sir.meepington.1@gmail.com>
2019-11-09 14:13:18 +0000

    Add GuestBooking addition, etc

---
+++ ThAmCo.Events/Controllers/GuestBookingsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Controllers
+{
+    public class GuestBookingsController : Controller
+    {
+        private readonly EventsDbContext _context;
+
+        public GuestBookingsController(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GuestBookings
+        public async Task<IActionResult> Index(int? id)
+        {
+            var eventsDbContext = _context.Guests.Include(g => g.Customer).Include(g => g.Event);
+            if (id.HasValue)
+            {
+                Event ev = await _context.Events.FirstOrDefaultAsync(e => e.Id == id);
+                if (ev != null)
+                {
+                    ViewData["Title"] = "Guest Bookings for "+ev.Title;
+                    var outList = await eventsDbContext.Where(g => g.EventId == ev.Id).ToListAsync();
+                    return View(outList);
+                }
+            }
+            ViewData["Title"] = "All Guest Bookings";
+            return View(await eventsDbContext.ToListAsync());
+        }
+
+        // GET: GuestBookings/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email");
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Title");
+            return View();
+        }
+
+        // POST: GuestBookings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,EventId,Attended")] GuestBooking guestBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(guestBooking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", guestBooking.CustomerId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Title", guestBooking.EventId);
+            return View(guestBooking);
+        }
+
+        // GET: GuestBookings/Edit/5
+        public async Task<IActionResult> Attend(int? id, int? customerId)
+        {
+            if (id == null || customerId == null) 
+            {
+                return NotFound();
+            }
+
+            GuestBooking booking = await _context.Guests.FindAsync(id, customerId);
+            if (booking == null)
+                return NotFound();
+
+            if (booking.Attended == true) // Already attended
+                return RedirectToAction(nameof(Index), new { id });
+
+            booking.Attended = true; // Attend 
+            _context.Update(booking);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index), new { id });
+
+        }
+
+        // GET: GuestBookings/Delete/5
+        public async Task<IActionResult> Remove(int? id, int? customerId)
+        {
+            if (id == null || customerId == null)
+            {
+                return NotFound();
+            }
+
+            var guestBooking = await _context.Guests
+                .Include(g => g.Customer)
+                .Include(g => g.Event)
+                .FirstOrDefaultAsync(m => m.CustomerId == id && m.EventId == customerId);
+            if (guestBooking == null)
+            {
+                return NotFound();
+            }
+
+            return View(guestBooking);
+        }
+
+        // POST: GuestBookings/Delete/5
+        [HttpPost, ActionName("Remove")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveConfirmed(int id, int customerId)
+        {
+            var guestBooking = await _context.Guests.FindAsync(id, customerId);
+            _context.Guests.Remove(guestBooking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- ThAmCo.Events/Data/GuestBooking.cs
+++ ThAmCo.Events/Data/GuestBooking.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ThAmCo.Events.Data
 {
     public class GuestBooking
     {
+        [Key, Column(Order = 1)]
         public int CustomerId { get; set; }
 
         public Customer Customer { get; set; }
 
+        [Key, Column(Order = 0)]
         public int EventId { get; set; }
 
         public Event Event { get; set; }
