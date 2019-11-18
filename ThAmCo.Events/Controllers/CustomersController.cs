b6a0402cce1ac18679c381133aac0b50454dfd02
Aidan Crooks <sir.meepington.1@gmail.com>
2019-11-18 14:47:01 +0000

    Customers details. Re-add customer controller?

---
+++ ThAmCo.Events/Controllers/CustomersController.cs
@@ -0,0 +1,187 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ThAmCo.Events.Data;
+using ThAmCo.Events.Models;
+
+namespace ThAmCo.Events.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly EventsDbContext _context;
+
+        public CustomersController(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+
+        // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Customer customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            CustomerDetailsViewModel model = new CustomerDetailsViewModel()
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                Surname = customer.FirstName,
+                Email = customer.Email,
+                Bookings = await _context.Guests
+                                .Include(b => b.Customer)
+                                .Include(b => b.Event)
+                                .Where(i => i.CustomerId == customer.Id)
+                                .ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Surname,FirstName,Email")] CustomerCreateViewModel customer)
+        {
+            if (ModelState.IsValid)
+            {
+                Customer model = new Customer()
+                {
+                    Id = customer.Id,
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    Surname = customer.Surname
+                };
+                _context.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Customer customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            CustomerEditViewModel model = new CustomerEditViewModel()
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                Surname = customer.Surname,
+                Email = customer.Email
+            };
+
+            return View(model);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Surname,FirstName,Email")] CustomerEditViewModel customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Customer currentCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Id == customer.Id);
+                    if (currentCustomer == null)
+                        return BadRequest();
+
+                    currentCustomer.Email = customer.Email;
+                    currentCustomer.FirstName = customer.FirstName;
+                    currentCustomer.Surname = customer.Surname;
+
+                    _context.Update(currentCustomer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Customer customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Customer customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ThAmCo.Events/Models/CustomerDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Models
+{
+    public class CustomerDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public IEnumerable<GuestBooking> Bookings { get; set; }
+
+    }
+}
